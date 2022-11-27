0edb358fd3c3421862a4900b95c03624ef986be7
BlackShark <Bl4ckSh4rk@users.noreply.github.com>
2022-11-27 21:15:15 +0100

    Updated for PKHeX 22.11.26

--- MirageIslandPlugin/MirageIslandPlugin.cs
+++ MirageIslandPlugin/MirageIslandPlugin.cs
@@ -9,51 +9,43 @@ namespace MirageIslandPlugin
         public string Name => "Mirage Island Tool";
         public int Priority => 1; // Loading order, lowest is first.
         public ISaveFileProvider SaveFileEditor { get; private set; } = null!;
-        public IPKMView PKMEditor { get; private set; } = null!;
 
         private ToolStripMenuItem? ctrl;
 
         public void Initialize(params object[] args)
         {
             Console.WriteLine($"Loading {Name}...");
-            if (args == null)
-                return;
             SaveFileEditor = (ISaveFileProvider)Array.Find(args, z => z is ISaveFileProvider)!;
-            var menu = (ToolStrip)Array.Find(args, z => z is ToolStrip);
-            LoadMenuStrip(menu);
+            LoadMenuStrip((ToolStrip)Array.Find(args, z => z is ToolStrip));
         }
 
         private void LoadMenuStrip(ToolStrip menuStrip)
         {
-            var items = menuStrip.Items;
-            var tools = (ToolStripDropDownItem)items.Find("Menu_Tools", false)[0];
-            AddPluginControl(tools);
+            AddPluginControl((ToolStripDropDownItem)menuStrip.Items.Find("Menu_Tools", false)[0]);
         }
 
         private void AddPluginControl(ToolStripDropDownItem tools)
         {
-            ctrl = new ToolStripMenuItem(Name);
-            tools.DropDownItems.Add(ctrl);
-            ctrl.Image = Properties.Resources.icon;
-            ctrl.Click += new EventHandler(OpenFeebasLocatorForm);
-            ctrl.Enabled = false;
+            ctrl = new(Name)
+            {
+                Visible = false,
+                Image = Properties.Resources.icon
+            };
+            ctrl.Click += new(OpenFeebasLocatorForm);
+            _ = tools.DropDownItems.Add(ctrl);
         }
 
         private void OpenFeebasLocatorForm(object sender, EventArgs e)
         {
-            using MirageIslandForm mif = new((SAV3)SaveFileEditor.SAV);
-            mif.ShowDialog();
+            _ = new MirageIslandForm((SAV3)SaveFileEditor.SAV).ShowDialog();
         }
 
         public void NotifySaveLoaded()
         {
             if (ctrl != null)
-                ctrl.Enabled = SaveFileEditor.SAV is SAV3RS or SAV3E;
+                ctrl.Visible = SaveFileEditor.SAV is SAV3RS or SAV3E;
         }
-        public bool TryLoadFile(string filePath)
-        {
-            Console.WriteLine($"{Name} was provided with the file path, but chose to do nothing with it.");
-            return false; // no action taken
-        }
+
+        public bool TryLoadFile(string filePath) => false;
     }
 }
